c15d3d3e60ea6a03614a2609a7c358d34185dbf1
Attila Szucs <attila.szucs@gmail.com>
2021-09-24 10:44:08 +0200

    Fix security warnings

--- part4/kafka4/src/test/java/org/example/AppTest.java
+++ part4/kafka4/src/test/java/org/example/AppTest.java
@@ -18,6 +18,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
